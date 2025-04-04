7df37b427401325bce23f96abb9c585d2fa07b38
David Rodriguez <admin@conofamily.org>
2025-04-04 01:08:37 -0600

    CrypterCLI: Crypter finished.

---
+++ Crypter CLI/Crypter CLI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.CommandLine;
+using CrypterCLI;
+
+
+RootCommand rootCommand = new RootCommand("Crypter CLI - Encrypt, decrypt, and hash data");
+
+
+rootCommand.AddCommand(EncryptCommand.CreateCommand());
+rootCommand.AddCommand(DecryptCommand.CreateCommand());
+rootCommand.AddCommand(HashCommand.CreateCommand());
+
+
+rootCommand.Invoke(args);
+
+// For testing purposes, you can uncomment the line below
+// rootCommand.Invoke(new string[] { "encrypt", "--input", "Hello World", "--text", "--password", "secret123" });
